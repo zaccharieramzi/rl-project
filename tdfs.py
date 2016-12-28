8715daf9fc476d3419a112ac239090590536b421
zaccharieramzi <zaccharie.ramzi@gmail.com>
2016-12-28 13:04:15 +0100

    added possibility to use Thompson-sampling, wrapped script in function

--- tdfs.py
+++ tdfs.py
@@ -12,14 +12,7 @@ from arms import ArmBernoulli
 n_users = 3
 n_arms = 5
 t_horizon = 1000
-
 arm_means = [0.2, 0.3, 0.5, 0.8, 0.9]
-
-arms = list()
-for i in range(n_arms):
-    arms.append(ArmBernoulli(arm_means[i]))
-
-# ALGORITHM PARAMETERS
 
 
 class SecondaryUser:
@@ -34,8 +27,14 @@ class SecondaryUser:
         self.draws = np.zeros(n_arms)
         self.collided_in_subsequence = False
 
-    def decision(self, t):
-        ''' Choses the arm to draw at each time step t.
+    def decision(self, t, alg='ucb'):
+        if alg == 'ucb':
+            return self.decision_ucb(t)
+        elif alg == 'ts':
+            return self.decision_ts(t)
+
+    def decision_ucb(self, t):
+        ''' Choses the arm to draw at each time step t, following UCB.
             Args:
                 - t (int): the time step.
             Output:
@@ -55,35 +54,57 @@ class SecondaryUser:
             arms_sorted = arms_sorted[::-1]
             return arms_sorted[top_arm_to_consider]
 
+    def decision_ts(self, t):
+        ''' Choses the arm to draw at each time step t, following TS.
+            Args:
+                - t (int): the time step.
+            Output:
+                - int: the index of the arm chosen by this user.
+        '''
+        betas = np.zeros(self.n_arms)
+        for arm_id in range(n_arms):
+            betas(arm_id) = np.random.beta(
+                np.sum(self.rewards[arm_id, :]) + 1,
+                self.draws[arm_id] - np.sum(self.rewards[arm_id, :]) + 1)
+        return np.argmax(betas)
 
-users = [SecondaryUser(n_arms, n_users) for i in range(n_users)]
-total_rewards = np.zeros((t_horizon, 1))
-for t in range(t_horizon):
-    choices = [user.decision(t) for user in users]
-    choice_count = collections.Counter(choices)
-    collisioned_users_id = (user_id for (user_id, choice) in enumerate(choices)
-                            if choice_count[choice] > 1)
-    for user_id in collisioned_users_id:
-        users[user_id].collided_in_subsequence = True
-    arms_to_draw = [choice for choice in choices if choice_count[choice] == 1]
-    for (user_id, choice) in enumerate(choices):
-        if choice in arms_to_draw:
-            arm = arms[choice]
-            user = users[user_id]
-            reward = arm.draw()
-            user.draws[choice] += 1
-            user.rewards[choice, t] = reward
-            total_rewards[t] += reward
-    if (t == n_arms) or ((t > n_arms) and ((t - n_arms) % n_users == 0)):
-        # We are at the end of a subsequence corresponding to
-        # initialization or classic. Therefore, we must correct the offsets.
-        for user in users:
-            if user.collided_in_subsequence:
-                user.offset = random.randint(0, user.n_users - 1)
-                user.collided_in_subsequence = False
 
-best_arms = np.sort(np.array(arm_means))[-n_users:]
-regret = np.cumsum(best_arms.sum() - total_rewards)
-plt.plot(range(t_horizon), regret, linewidth=2)
-plt.legend("regret")
-plt.show()
+def tdfs_routine(n_users, n_arms, t_horizon, arm_means, alg='ucb', plot=False):
+    arms = list()
+    for i in range(n_arms):
+        arms.append(ArmBernoulli(arm_means[i]))
+    users = [SecondaryUser(n_arms, n_users) for i in range(n_users)]
+    total_rewards = np.zeros((t_horizon, 1))
+    for t in range(t_horizon):
+        choices = [user.decision(t, alg=alg) for user in users]
+        choice_count = collections.Counter(choices)
+        collisioned_users_id = (user_id for (user_id, choice)
+                                in enumerate(choices)
+                                if choice_count[choice] > 1)
+        for user_id in collisioned_users_id:
+            users[user_id].collided_in_subsequence = True
+        arms_to_draw = [choice for choice in choices
+                        if choice_count[choice] == 1]
+        for (user_id, choice) in enumerate(choices):
+            if choice in arms_to_draw:
+                arm = arms[choice]
+                user = users[user_id]
+                reward = arm.draw()
+                user.draws[choice] += 1
+                user.rewards[choice, t] = reward
+                total_rewards[t] += reward
+        if (t == n_arms) or ((t > n_arms) and ((t - n_arms) % n_users == 0)):
+            # We are at the end of a subsequence corresponding to
+            # initialization or classic.
+            # Therefore, we must correct the offsets.
+            for user in users:
+                if user.collided_in_subsequence:
+                    user.offset = random.randint(0, user.n_users - 1)
+                    user.collided_in_subsequence = False
+    if plot:
+        best_arms = np.sort(np.array(arm_means))[-n_users:]
+        regret = np.cumsum(best_arms.sum() - total_rewards)
+        plt.plot(range(t_horizon), regret, linewidth=2)
+        plt.legend("regret")
+        plt.show()
+    return total_rewards
