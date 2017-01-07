635a42e73c468cd4310bdc6fb10d729b369eaf90
tournemire <pierredet@gmail.com>
2017-01-07 13:20:08 +0100

    variable number of users POC

--- mega/routines.py
+++ mega/routines.py
@@ -5,7 +5,8 @@ from arms import ArmBernoulli
 from .users import SecondaryUser, UCBUser, TSUser
 
 
-def mega_routine(n_users, params, n_arms, t_horizon, arm_means, alg='ucb'):
+def mega_routine(n_users, params, n_arms, t_horizon, arm_means, alg='ucb',
+                 variable=False):
 
     arms = [ArmBernoulli(mean) for mean in arm_means]
 
@@ -21,16 +22,27 @@ def mega_routine(n_users, params, n_arms, t_horizon, arm_means, alg='ucb'):
     rewards = np.zeros((n_users, t_horizon))
     collisions = np.zeros((n_users, t_horizon), dtype=np.uint8)
     occupation = np.zeros(n_users)
+    nbr_of_users = np.zeros(t_horizon, dtype="int")
     # main loop
     for t in range(1, t_horizon):
+
+        if variable:
+            # if the number is variable we redraw active users 10 times
+            if t % (t_horizon // 10) == 1:
+                active_users = draw_active_users(users, algMap[alg], n_arms,
+                                                 params, occupation)
+                print("{:d} active users !".format(len(active_users)))
+        else:
+            # otherwise everybody participates !
+            active_users = enumerate(users)
+        nbr_of_users[t] = len(active_users)
         # update collision status
-        for i, user in enumerate(users):
+        for i, user in active_users:
             if not user.collided:
                 # increase its persistence probability
                 user.persistence_proba *= user.params['alpha']
                 user.persistence_proba += user.params['alpha']
             else:
-                collisions[i, t-1] = 1
                 if random.random() < user.persistence_proba:
                     # the users persists !
                     user.previous_arm = user.arm
@@ -49,16 +61,34 @@ def mega_routine(n_users, params, n_arms, t_horizon, arm_means, alg='ucb'):
                             occupation == user.arm)[0][0]].collided = False
 
         # non collided users make a move
-        for i, user in enumerate(users):
+        for i, user in active_users:
             if not user.collided:
                 # update users whishes
                 occupation[i] = user.decision(t)
 
         # reward and collisions
-        for i, user in enumerate(users):
+        for i, user in active_users:
             if (occupation == user.arm).sum() > 1:
                 user.collided = True
-
+                collisions[i, t] = 1
             else:
                 rewards[i, t] = user.draw_from_arm(arms[user.arm])
+    if variable:
+        return rewards, collisions, nbr_of_users
     return rewards, collisions
+
+
+def draw_active_users(users, alg, n_arms, params, occupation):
+    """
+    return a subset of the (indices, users) active at time t
+    """
+
+    active_users = []
+    for i, user in enumerate(users):
+        if random.random() > 0.2:
+            active_users.append((i, user))
+        else:
+            # others are reset
+            user = alg(n_arms, params)
+            occupation[i] = -1
+    return active_users
